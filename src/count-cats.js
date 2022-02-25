7a061dca56cdaf96b3f669d95e2a97bdaa93dac7
rydvone <rydvone@gmail.com>
2022-02-25 10:25:24 +0300

    refactor: count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,23 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  let numCats = "";
-  console.log('arguments' + arguments);
-  for (let item of arguments) {
-    for (let inde of item) {
-      // console.log(inde);
-      // numCats += inde.filter((el) => el == "^^");
-      // console.log(numCats);
-      for (let el of inde) {
-        if (el == "^^") {
-          numCats++;
-        }
+function countCats() {
+  let numCats = 0;
+  for (let item of arguments[0]) {
+    for (let el of item) {
+      if (el == "^^") {
+        numCats++;
       }
     }
   }
-  console.log("numCats = " + numCats);
-  return numCats; 
+  return numCats;
 }
 
 module.exports = {
