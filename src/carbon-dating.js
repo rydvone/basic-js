b2d844c54a727b5f45eb6004db40a7cf75651db8
rydvone <rydvone@gmail.com>
2022-03-07 16:04:28 +0300

    feat: carbon-dating is done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,27 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  let k,
-      temp,
-      res;
-    
-    if (typeof(abs) === 'string') {
-    // k = 1.22e-4;
-    k = 0.693 / HALF_LIFE_PERIOD;
-    k = Math.round(k * 1000000) / 1000000;
-    console.log('k = ' + k);
-    temp = Math.log(MODERN_ACTIVITY / abs);
-    temp = Math.ceil(temp * 1000) / 1000;
-    console.log('temp = ' + temp);
-    res = Math.ceil(temp / k);
-    console.log('res = ' + res);
-    return res;  
+function dateSample() {
+  // throw new NotImplementedError('Not implemented');
+  let res;
+
+  if (typeof (arguments[0]) === 'string' && !(isNaN(arguments[0])) && arguments[0] > 0 && arguments[0] < MODERN_ACTIVITY) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / arguments[0]) / (0.693 / HALF_LIFE_PERIOD));
+    // return res;
   } else {
     return false;
   }
-   
 }
 
 module.exports = {
