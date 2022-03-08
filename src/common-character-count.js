b11d4c008829ff1c569d026f6e1016531258b20e
rydvone <rydvone@gmail.com>
2022-03-08 10:32:44 +0300

    feat: delete-digit

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,7 +12,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
-  // throw new NotImplementedError('Not implemented');
+  throw new NotImplementedError('Not implemented');
   let res = 0;
   const arr1 = s1.split('');
   const arr2 = s2.split('');
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(num) {
+  // throw new NotImplementedError('Not implemented');
+  let arr = [];
+  num = '' + num;
+  for(let i = 0; i < num.length; i++) {
+    let arrTemp = ('' + num).split('');
+    arrTemp.splice(i, 1);
+    arr.push(arrTemp.join(''));
+  }
+  return Math.max(...arr);
 }
 
 module.exports = {
