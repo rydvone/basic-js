3298e7627e930b8ce5f18d3585a970a4b1c67930
rydvone <rydvone@gmail.com>
2022-04-30 16:26:13 +0300

    feat: vigenere-cipher is done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,114 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(checkDirection) {
+    this.checkDirection = checkDirection;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    // throw new NotImplementedError('Not implemented');
+    if (typeof message === 'undefined' || typeof key === 'undefined') {
+      throw new Error("Incorrect arguments!");
+    }
+    const tableVigenere = function (firstLetter, secondLetter) {
+      const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+        'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+        'W', 'X', 'Y', 'Z']
+      let table = [];
+
+      for (let i = 0; i < alphabet.length; i++) {
+        let alphabetInner = [...alphabet];
+        let temp = alphabetInner.splice(0, i);
+
+        alphabetInner.push(...temp);
+        table.push(alphabetInner);
+      }
+
+      let numLetterFirst = alphabet.indexOf(firstLetter);
+      let numLetterSecond = alphabet.indexOf(secondLetter);
+      let resultLetter = table[numLetterFirst][numLetterSecond];
+      return resultLetter;
+    }
+
+    const regEngSymb = /([A-Za-z])/gi;
+    let messageUpper = message.replace(regEngSymb, (el) => el.toUpperCase());
+    if (this.checkDirection === false) {
+      let reversedMessageTemp = messageUpper.split('');
+      messageUpper = reversedMessageTemp.reverse().join('');
+    }
+
+    let messageOnlyLetterInner = messageUpper.match(regEngSymb);
+    let keyUpper = key.replace(regEngSymb, (el) => el.toUpperCase());
+    let countRepeatKey = Math.ceil(messageOnlyLetterInner.length / key.length);
+    let keyOnlyLetterInner = keyUpper.repeat(countRepeatKey).slice(0, messageOnlyLetterInner.length).split('');
+
+    if (this.checkDirection === false) {
+      keyOnlyLetterInner.reverse();
+    }
+
+    let resultOnlyLetterInner = [];
+    for (let i = 0; i < messageOnlyLetterInner.length; i++) {
+      resultOnlyLetterInner.push(tableVigenere(messageOnlyLetterInner[i], keyOnlyLetterInner[i]))
+    }
+
+    let resultInner = messageUpper.replace(regEngSymb, (el) => {
+      return el = resultOnlyLetterInner.shift();
+    });
+    let resultWord = resultInner;
+    return resultWord;
+  }
+
+
+  decrypt(message = this.message, key = this.key) {
+    // throw new NotImplementedError('Not implemented');
+    if (typeof message === 'undefined' || typeof key === 'undefined') {
+      throw new Error("Incorrect arguments!");
+    }
+    const tableVigenereDecrypt = function (firstLetter, secondLetter) {
+      const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+        'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+        'W', 'X', 'Y', 'Z']
+      let table = [];
+
+      for (let i = 0; i < alphabet.length; i++) {
+        let alphabetInner = [...alphabet];
+        let temp = alphabetInner.splice(0, i);
+
+        alphabetInner.push(...temp);
+        table.push(alphabetInner);
+      }
+
+      let numLetterSecond = alphabet.indexOf(secondLetter);
+      let numLetterFirst = table[numLetterSecond].indexOf(firstLetter);
+      let resultLetter = alphabet[numLetterFirst];
+      return resultLetter;
+    }
+
+    const regEngSymb = /([A-Za-z])/gi;
+    let messageUpper = message.replace(regEngSymb, (el) => el.toUpperCase());
+    if (this.checkDirection === false) {
+      let reversedMessageTemp = messageUpper.split('');
+      messageUpper = reversedMessageTemp.reverse().join('');
+    }
+
+    let messageOnlyLetterInner = messageUpper.match(regEngSymb);
+    let keyUpper = key.replace(regEngSymb, (el) => el.toUpperCase());
+    let countRepeatKey = Math.ceil(messageOnlyLetterInner.length / key.length);
+    let keyOnlyLetterInner = keyUpper.repeat(countRepeatKey).slice(0, messageOnlyLetterInner.length).split('');
+
+    if (this.checkDirection === false) {
+      keyOnlyLetterInner.reverse();
+    }
+    let resultOnlyLetterInner = [];
+    for (let i = 0; i < messageOnlyLetterInner.length; i++) {
+      resultOnlyLetterInner.push(tableVigenereDecrypt(messageOnlyLetterInner[i], keyOnlyLetterInner[i]))
+    }
+
+    let resultInner = messageUpper.replace(regEngSymb, (el) => {
+      return el = resultOnlyLetterInner.shift();
+    });
+    let resultWord = resultInner;
+    return resultWord;
   }
 }
 
